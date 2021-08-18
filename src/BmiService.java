9b821811a725a181a3ce24f89b13f7c4f9a219ed
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-08-18 23:55:43 +0300

    стиль

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public float calculate(float mass, float height) {
-        float res= mass/(float) Math.pow(height, 2);
+        float res = mass / (float) Math.pow(height, 2);
         return res;
     }
 }
